5029408ddc09ca58cf5fa5733affa635dd17c8ca
Rodionovmax <mrodionov@connectrn.com>
2023-02-26 12:04:10 -0500

    init project

---
+++ app/src/main/java/com/rodionovmax/onedron/HomeFragment.kt
@@ -0,0 +1,44 @@
+package com.rodionovmax.onedron
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.rodionovmax.onedron.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
